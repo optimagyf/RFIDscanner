7c43f0f33a63348ce5788299673a5d2b53fdbc00
frozar <fabien.rozar@gmail.com>
2021-05-03 19:12:52 +0400

    Adjust pin number usage.

--- nodeMCULolinV3_ESP8266/include/wifiUtils.h
+++ nodeMCULolinV3_ESP8266/include/wifiUtils.h
@@ -149,19 +149,23 @@ FS *filesystem = &SPIFFS;
 #define PIN_SDA 21 // Pin SDA mapped to pin GPIO21/SDA of ESP32
 #else
 //PIN_D0 can't be used for PWM/I2C
-#define PIN_D0 16  // Pin D0 mapped to pin GPIO16/USER/WAKE of ESP8266. This pin is also used for Onboard-Blue LED. PIN_D0 = 0 => LED ON
+#define PIN_D0 16  // Pin D0 mapped to pin GPIO16/USER/WAKE of ESP8266. \
+                   // This pin is also used for Onboard-Blue LED.       \
+                   // PIN_D0 = 0 => LED ON
 #define PIN_D1 5   // Pin D1 mapped to pin GPIO5 of ESP8266
 #define PIN_D2 4   // Pin D2 mapped to pin GPIO4 of ESP8266
 #define PIN_D3 0   // Pin D3 mapped to pin GPIO0/FLASH of ESP8266
-#define PIN_D4 2   // Pin D4 mapped to pin GPIO2/TXD1 of ESP8266
-#define PIN_LED 2  // Pin D4 mapped to pin GPIO2/TXD1 of ESP8266, NodeMCU and WeMoS, control on-board LED
+#define PIN_D4 2   // Pin D4 mapped to pin GPIO2/TXD1 of ESP8266, \
+                   // NodeMCU and WeMoS, control on-board LED
 #define PIN_D5 14  // Pin D5 mapped to pin GPIO14/HSCLK of ESP8266
 #define PIN_D6 12  // Pin D6 mapped to pin GPIO12/HMISO of ESP8266
 #define PIN_D7 13  // Pin D7 mapped to pin GPIO13/RXD2/HMOSI of ESP8266
 #define PIN_D8 15  // Pin D8 mapped to pin GPIO15/TXD2/HCS of ESP8266
 
-//Don't use pins GPIO6 to GPIO11 as already connected to flash, etc. Use them can crash the program
-//GPIO9(D11/SD2) and GPIO11 can be used only if flash in DIO mode ( not the default QIO mode)
+// Don't use pins GPIO6 to GPIO11 as already connected to flash, etc.
+// Use them can crash the program
+// GPIO9(D11/SD2) and GPIO11 can be used only if flash in DIO mode
+// (not the default QIO mode)
 #define PIN_D11 9  // Pin D11/SD2 mapped to pin GPIO9/SDD2 of ESP8266
 #define PIN_D12 10 // Pin D12/SD3 mapped to pin GPIO10/SDD3 of ESP8266
 #define PIN_SD2 9  // Pin SD2 mapped to pin GPIO9/SDD2 of ESP8266
@@ -172,7 +176,8 @@ FS *filesystem = &SPIFFS;
 #define PIN_RX 3  // Pin RX mapped to pin GPIO3/RXD0 of ESP8266
 #define PIN_TX 1  // Pin RX mapped to pin GPIO1/TXD0 of ESP8266
 
-#define LED_PIN 16 // Pin D0 mapped to pin GPIO16 of ESP8266. This pin is also used for Onboard-Blue LED. PIN_D0 = 0 => LED ON
+// #define LED_PIN 16 // Pin D0 mapped to pin GPIO16 of ESP8266. This pin is also used for Onboard-Blue LED. PIN_D0 = 0 => LED ON
+#define PIN_LED PIN_D0
 
 #endif // ESP32
 
@@ -214,21 +219,21 @@ const int TRIGGER_PIN2 = PIN_D7; // D7 on NodeMCU and WeMos.
 
 typedef struct
 {
-  char wifi_ssid[SSID_MAX_LEN];
-  char wifi_pw[PASS_MAX_LEN];
+   char wifi_ssid[SSID_MAX_LEN];
+   char wifi_pw[PASS_MAX_LEN];
 } WiFi_Credentials;
 
 typedef struct
 {
-  String wifi_ssid;
-  String wifi_pw;
+   String wifi_ssid;
+   String wifi_pw;
 } WiFi_Credentials_String;
 
 #define NUM_WIFI_CREDENTIALS 2
 
 typedef struct
 {
-  WiFi_Credentials WiFi_Creds[NUM_WIFI_CREDENTIALS];
+   WiFi_Credentials WiFi_Creds[NUM_WIFI_CREDENTIALS];
 } WM_Config;
 
 // WM_Config WM_config;
--- nodeMCULolinV3_ESP8266/src/main.cpp
+++ nodeMCULolinV3_ESP8266/src/main.cpp
@@ -48,14 +48,46 @@
    can not be read by observers.
  *****************************************************************************************************************************/
 
+/*
+ * --------------------------------------------------------------------------------------------------------------------
+ * Example sketch/program showing how to read data from a PICC to serial.
+ * --------------------------------------------------------------------------------------------------------------------
+ * This is a MFRC522 library example; for further details and other examples see: https://github.com/miguelbalboa/rfid
+ * 
+ * Example sketch/program showing how to read data from a PICC (that is: a RFID Tag or Card) using a MFRC522 based RFID
+ * Reader on the Arduino SPI interface.
+ * 
+ * When the Arduino and the MFRC522 module are connected (see the pin layout below), load this sketch into Arduino IDE
+ * then verify/compile and upload it. To see the output: use Tools, Serial Monitor of the IDE (hit Ctrl+Shft+M). When
+ * you present a PICC (that is: a RFID Tag or Card) at reading distance of the MFRC522 Reader/PCD, the serial output
+ * will show the ID/UID, type and any data blocks it can read. Note: you may see "Timeout in communication" messages
+ * when removing the PICC from reading distance too early.
+ * 
+ * If your reader supports it, this sketch/program will read all the PICCs presented (that is: multiple tag reading).
+ * So if you stack two or more PICCs on top of each other and present them to the reader, it will first output all
+ * details of the first and then the next PICC. Note that this may take some time as all data blocks are dumped, so
+ * keep the PICCs at reading distance until complete.
+ * 
+ * @license Released into the public domain.
+ * 
+ * Typical pin layout used:
+ * ------------------------------------------------------------------------------------------------------------
+ *             MFRC522      NodeMCU Lolin      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   V3 Module          Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          ESP8266 (ESP-12F)  Pin           Pin       Pin        Pin              Pin
+ * ------------------------------------------------------------------------------------------------------------
+ * RST/Reset   RST          D3                 9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      D4                 10            53        D10        10               10
+ * SPI MOSI    MOSI         D7                 11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         D6                 12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          D5                 13 / ICSP-3   52        D13        ICSP-3           15
+ */
 #include "wifiUtils.h"
 
 bool triggerPortalConfiguration = false;
 
 void ICACHE_RAM_ATTR handleInterrupt()
 {
-  printf("\n");
-  printf("handleInterrupt %lu\n\n", millis());
   triggerPortalConfiguration = true;
 }
 
@@ -64,8 +96,7 @@ void setup()
   // put your setup code here, to run once:
   // initialize the LED digital pin as an output.
   pinMode(PIN_LED, OUTPUT);
-  pinMode(TRIGGER_PIN, INPUT_PULLUP);
-  pinMode(TRIGGER_PIN2, INPUT_PULLUP);
+  pinMode(PIN_D1, INPUT_PULLUP);
 
   Serial.begin(115200);
   while (!Serial)
@@ -73,7 +104,7 @@ void setup()
 
   setupWifi();
 
-  attachInterrupt(digitalPinToInterrupt(PIN_D2), handleInterrupt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_D1), handleInterrupt, FALLING);
 }
 
 void loop()
